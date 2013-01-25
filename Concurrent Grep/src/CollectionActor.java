513645acd761bfe4eb30b6d0765c4410ee080be2
Cory <cms5822@rit.edu>
2013-01-25 17:59:15 -0500

    CollectionActor
    Implemented basic functionality, however, can't figure out the shutdown
    command

--- Concurrent Grep/src/CollectionActor.java
+++ Concurrent Grep/src/CollectionActor.java
@@ -1,4 +1,33 @@
+import akka.actor.UntypedActor;
+import static akka.actor.Actors.*;
 
-public class CollectionActor {
+public class CollectionActor extends UntypedActor{
+	
+	private boolean receiveFileCount;
+	private int numberOfFiles;
+	public CollectionActor(){
+		receiveFileCount = false; 
+	}
+	
+	public void onReceive(Object message) throws Exception {
+		if(message instanceof FileCount && !receiveFileCount){
+			receiveFileCount = true;
+			FileCount tempMessage = (FileCount) message;
+			numberOfFiles = tempMessage.getNumberOfFiles();
+		}
+		else if(message instanceof Found){
+			Found tempMessage = (Found) message;
+			System.out.println(tempMessage.getFileName());
+			for(String s : tempMessage.getMatchedLines()){
+				System.out.println(s);
+			}
+			numberOfFiles--;
+			
+			if(numberOfFiles < 1){
+				//shutdown actor: can not figure out how to call method
+				//Actors.registry().shutdownAll();
+			}
 
+		}
+	}
 }
