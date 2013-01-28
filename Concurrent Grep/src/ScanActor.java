1e7b219e5a9ef658a07fbc1aa72c80d135bffe81
Chris <celdridge91190@gmail.com>
2013-01-28 16:44:19 -0500

    Added handling for standard input

--- Concurrent Grep/src/ScanActor.java
+++ Concurrent Grep/src/ScanActor.java
@@ -12,30 +12,47 @@ import java.util.regex.Pattern;
 public class ScanActor extends UntypedActor{
 
 	ArrayList<String> matchLines = new ArrayList<String>();
-	
+
 	@Override
 	public void onReceive(Object message) throws Exception
 	{
 		String fileName;
 		if (message instanceof Configure)
 		{
-			Configure thisMessage = (Configure)message; 
-			fileName = thisMessage.getFile();
-			ActorRef collector = thisMessage.getCollectionActor();
-			File file = new File(fileName);
-			parseFile(file);
-			Found found = new Found(fileName, matchLines);
-			collector.tell(found);
+			Configure thisMessage = (Configure)message;
+			if (thisMessage.getFile() == null)
+			{
+				parseInput();
+			}
+			else
+			{
+				fileName = thisMessage.getFile();
+				ActorRef collector = thisMessage.getCollectionActor();
+				File file = new File(fileName);
+				parseFile(file);
+				Found found = new Found(fileName, matchLines);
+				collector.tell(found);
+			}
 		}
 		else
 		{
 			throw new IllegalArgumentException("Wrong type of message");
 		}
-		
-		
-		
+
+
+
 	}
-	
+
+	public void parseInput()
+	{
+		Scanner scanner = new Scanner(System.in);
+		String line;
+		while(scanner.hasNext())
+		{
+			line = scanner.nextLine();
+			checkLine(line);
+		}
+	}
 	public void parseFile(File checkFile) throws FileNotFoundException
 	{
 		Scanner scanner = new Scanner(checkFile);
@@ -45,19 +62,19 @@ public class ScanActor extends UntypedActor{
 			line = scanner.nextLine();
 			checkLine(line);
 		}
-	
+
 	}
-	
+
 	public void checkLine(String checkLine)
 	{
 		int patternCount = 0;
 		Pattern pattern = Pattern.compile("ed");
 		Matcher matcher = pattern.matcher("ed");
-		
+
 		if (matcher.find())
 		{
 			matchLines.add(checkLine);
 		}
 	}
-	
+
 }
